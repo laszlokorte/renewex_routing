409f431bd35b689cad18314c3db99c1e1403d9ad
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-09-27 23:35:56 +0200

    initial commit

---
+++ lib/renewex_routing.ex
@@ -0,0 +1,18 @@
+defmodule RenewexRouting do
+  @moduledoc """
+  Documentation for `RenewexRouting`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RenewexRouting.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/renewex_routing_test.exs
@@ -0,0 +1,8 @@
+defmodule RenewexRoutingTest do
+  use ExUnit.Case
+  doctest RenewexRouting
+
+  test "greets the world" do
+    assert RenewexRouting.hello() == :world
+  end
+end
