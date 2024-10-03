87b3c910d2a310cb07881eb8d0fd0fa7f2a5f24f
Laszlo Korte <laszlokorte@users.noreply.github.com>
2024-10-03 18:38:30 +0200

    update version

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule RenewexRouting.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/laszlokorte/renewex_routing"
-  @version "0.1.0"
+  @version "0.2.0"
 
   def project do
     [
